ecf3c555334f28777c97bba96bb8c5444231dd2b
Gerard Andrés Venegas Vargas <gerarduc@gmail.com>
2023-04-04 22:28:36 -0400

    Primer push desde GitKraken

--- Clase/src/fin.java
+++ Clase/src/fin.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class fin {
+import java.util.Scanner;
+
+public class fin {
+
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        String esFin = "";
+
+        while (!esFin.equalsIgnoreCase("fin") ){
+            System.out.print("Ingrese una palabra: ");
+            esFin = sc.next();
+            System.out.println(esFin);
+        }
+
+        System.out.print("Gracias");
+    }
 }
